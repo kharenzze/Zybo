dea4781e17c4cab55a1d2ca717cce8247d866240
kharenzze <str237@gmail.com>
2015-01-23 10:05:24 +0100

    Ambos proyectos adaptados correctamente a vivado 2014.2

---
+++ zybo_base_system/source/vivado/hw/zybo_bsd/zybo_bsd.sdk/base_demo/Debug/src/timer_ps.d
@@ -0,0 +1,45 @@
+src/timer_ps.d: ../src/timer_ps.c ../src/timer_ps.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../base_demo_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../src/timer_ps.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../base_demo_bsp/ps7_cortexa9_0/include/xparameters.h:
